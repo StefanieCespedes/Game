cc2b8c00addb4c049bed33053c48f9df18ff97e9
Stefanie Cespedes <stefanie.davilaa@gmail.com>
2019-06-27 12:09:47 -0300

    design

--- DOM.js
+++ DOM.js
@@ -1,64 +1,66 @@
 
 let questionsArr = [
-  {question: 'qual eh?',
-    answer1: 'sim',
-    answer2: 'nao',
-    answer3: 'sim',
-    rightAnswer: 'sim',
+  {question: 'Which keyword removes the first element of an array?',
+    answer1: 'unshift',
+    answer2: 'shift',
+    answer3: 'push',
+    rightAnswer: 'shift',
   },
-  {question: 'qual foi?',
-    answer1: 'sim',
-    answer2: 'NAO',
-    answer3: 'sim',
-    rightAnswer: 'NAO',
+  {question: 'What do you call a value that is attached to an object?',
+    answer1: 'parameter',
+    answer2: 'variable',
+    answer3: 'property',
+    rightAnswer: 'property',
   },
-  {question: 'qual 3',
-    answer1: 'sim',
-    answer2: 'nao',
-    answer3: 'sim',
-    rightAnswer: 'sim',
+  {question: 'What is the keyword for converting "1.5" to 1?',
+    answer1: 'parseInt',
+    answer2: 'parseFloat',
+    answer3: 'Math',
+    rightAnswer: 'parseInt',
   },
-  {question: 'qual 4?',
-    answer1: 'sim',
-    answer2: 'nao',
-    answer3: 'sim',
-    rightAnswer: 'sim',
+  {question: `What is the value of num? let num = parseInt('5.6');`,
+    answer1: '6',
+    answer2: '5',
+    answer3: '5.5',
+    rightAnswer: '5',
   },
-  {question: 'qual 5',
-    answer1: 'sim',
-    answer2: 'nao',
-    answer3: 'sim',
-    rightAnswer: this.answer1,
+  {question: 'A function that creates objects and their properties is known as a',
+    answer1: 'Class',
+    answer2: 'Constructor',
+    answer3: 'Method',
+    rightAnswer: 'Constructor',
   },
-  {question: 'qual 6',
-    answer1: 'sim',
-    answer2: 'nao',
-    answer3: 'sim',
-    rightAnswer: this.answer1,
+  {question: 'Global and local variables differ in their',
+    answer1: 'scope',
+    answer2: 'relevance',
+    answer3: 'size',
+    rightAnswer: 'scope',
   },
-  {question: 'qual 7?',
-    answer1: 'sim',
-    answer2: 'nao',
-    answer3: 'sim',
-    rightAnswer: this.answer1,
+  {question: 'What is the keyword for removing one set of characters and inserting another set in its place?',
+    answer1: 'charAt() = ',
+    answer2: 'replace',
+    answer3: 'change',
+    rightAnswer: 'replace',
   },
-  {question: 'qual 8?',
-    answer1: 'sim',
-    answer2: 'nao',
-    answer3: 'sim',
-    rightAnswer: this.answer1,
+  {question: `After these two statements execute, what is the value of x?
+		var x = "abc";
+		var y = x.replace("a", "z");`,
+    answer1: 'zbc',
+    answer2: 'az',
+    answer3: 'abc',
+    rightAnswer: 'abc',
   },
-  {question: 'qual 9?',
-    answer1: 'sim',
-    answer2: 'nao',
-    answer3: 'sim',
-    rightAnswer: this.answer1,
+  {question: 'What is the keyword for detecting that the user is hovering over an element?',
+    answer1: 'onClick',
+    answer2: 'onMouseover',
+    answer3: 'hover',
+    rightAnswer: 'onMouseover',
   },
-  {question: 'qual 10?',
-    answer1: 'sim',
-    answer2: 'nao',
-    answer3: 'sim',
-    rightAnswer: this.answer1,
+  {question: 'If a function has 3 parameters, how many values can be returned to the calling code?',
+    answer1: '1',
+    answer2: '3',
+    answer3: '2',
+    rightAnswer: '1',
   },
 ];
 
@@ -74,6 +76,8 @@ let count = 0;
 let answers;
 let canCrash = true;
 let getTheDiv;
+let startButton;
+let gameStart;
 
 function ask (arr) {
 
@@ -84,51 +88,61 @@ function ask (arr) {
 
   getTheDiv = document.getElementById('questions')
   const questionsForm = document.createElement('form');
-  getTheDiv.appendChild(questionsForm);
+	getTheDiv.appendChild(questionsForm);
+	questionsForm.setAttribute('id', 'makeItPop')
   const legend = document.createElement('legend');
   questionsForm.appendChild(legend);
-  let questionHTML = document.createTextNode(question);
+	let questionHTML = document.createTextNode(question);
   legend.appendChild(questionHTML);
 
   const theInput = document.createElement('input');
   theInput.setAttribute('id', 'answer1');
   theInput.setAttribute('type', 'radio');
   theInput.setAttribute('value', answer1)
-  theInput.setAttribute('name', 'answer');
+	theInput.setAttribute('name', 'answer');
+	theInput.setAttribute('class', 'styleTheBox');
   questionsForm.appendChild(theInput);
   const label1 = document.createElement('label');
-  label1.setAttribute('for', 'answer1')
+	label1.setAttribute('for', 'answer1');
+	label1.setAttribute('class', 'container');
   let answer1HTML = document.createTextNode(answer1);
   label1.appendChild(answer1HTML);
-  questionsForm.appendChild(label1)
+	questionsForm.appendChild(label1);
 
 
   const theInput2 = document.createElement('input');
   theInput2.setAttribute('id', 'answer2');
   theInput2.setAttribute('type', 'radio');
-  theInput2.setAttribute('name', 'answer')
-  theInput2.setAttribute('value', answer2)
+  theInput2.setAttribute('name', 'answer');
+	theInput2.setAttribute('value', answer2);
+	theInput2.setAttribute('class', 'styleTheBox')
   questionsForm.appendChild(theInput2);
   const label2 = document.createElement('label');
-  label2.setAttribute('for', 'answer2');
+	label2.setAttribute('for', 'answer2');
+	label2.setAttribute('class', 'container');
   let answer2HTML = document.createTextNode(answer2);
   label2.appendChild(answer2HTML);
-  questionsForm.appendChild(label2);
+	questionsForm.appendChild(label2);
 
   const theInput3 = document.createElement('input');
   theInput3.setAttribute('id', 'answer3');
   theInput3.setAttribute('type', 'radio');
-  theInput3.setAttribute('name', 'answer')
-  theInput3.setAttribute('value', answer3)
+  theInput3.setAttribute('name', 'answer');
+	theInput3.setAttribute('value', answer3);
+	theInput3.setAttribute('class', 'styleTheBox');
   questionsForm.appendChild(theInput3);
   const label3 = document.createElement('label');
-  label3.setAttribute('for', 'answer3')
+	label3.setAttribute('for', 'answer3');
+	label3.setAttribute('class', 'container');
   let answer3HTML = document.createTextNode(answer3);
   label3.appendChild(answer3HTML);
-  questionsForm.appendChild(label3);
+	questionsForm.appendChild(label3);
 
-  const submitButton = document.createElement('button');
-  submitButton.setAttribute('onclick', 'getInfo()')
+	const submitButton = document.createElement('button');
+	submitButton.setAttribute('id', 'answerButton');
+	let buttonText = document.createTextNode('Send')
+	submitButton.appendChild(buttonText);
+  submitButton.setAttribute('onclick', 'getInfo()');
   submitButton.setAttribute('type', 'button');
   questionsForm.appendChild(submitButton);
 }
@@ -153,21 +167,29 @@ function getInfo () {
 		setTimeout(() => {
 			canCrash = true;
 		}, 2000);
+	} else {
+		lifes(n - 1);
 	}
 }
 
-// let radios = document.getElementsByName(radioName);
-// for (let i = 0; i < radios.length; i+=1) {
-// 	if(radios[i].checked)
-// 		return radios[i].value;
-// }
-// 	if (getInfo(questions[i] === answers[i]))
-// 	return `Congratulations! You are right`;
-// } else {
-// 	`You're wrong`;
-// }
-// 	const output = [];
-// }
+function startGame() {
+	gameStart = document.getElementById('design');
+	gameStart.setAttribute('class', 'start');
+	let gameName = document.createElement('h1');
+	let gameNameText = document.createTextNode('JS TRIVIA')
+	gameStart.appendChild(gameName);
+	gameName.setAttribute('id', 'textStyle');
+	gameName.appendChild(gameNameText)
+	startButton = document.createElement('button');
+	startButton.setAttribute('id', 'startIt');
+	startButton.setAttribute('onclick', 'button()');
+	// let textDiv = document.createElement('div');
+	// textDiv.setAttribute('class', 'lightning');
+	// gameStart.appendChild(textDiv);
+	let gameStartText = document.createTextNode('Start game!');
+	startButton.appendChild(gameStartText);
+	gameStart.appendChild(startButton);
+}
 
-//   condicional pra dizer se acertou
-//   volta jogo;
+
+startGame();
--- game.js
+++ game.js
@@ -1,26 +1,27 @@
 
+// some variables I'll need later
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 let frames = 0;
 let interval;
 const obstacles = [];
 const questions = [];
-canvas.width = 700;
-canvas.height = 500;
+canvas.width = 800;
+canvas.height = 600;
 
-// SCORE
 // GAMEOVER 
 // BOTAO DE INICIO
 // APRESENTACAO PERGUNTAS
 
+// prints the background image on canvas
 function drawBackground() {
-  ctx.beginPath();
-  ctx.fillStyle = '#FAF146';
-  ctx.fillRect(0, 0, 900, 600);
-  ctx.closePath();
+  let img = new Image();
+  img.src = './images/background2.png';
+  ctx.drawImage(img, 0, 0, 800, 600);
 }
 drawBackground();
 
+// a class for the player
 class Player {
   constructor(x, y, width, height) {
     this.x = x;
@@ -29,6 +30,7 @@ class Player {
     this.height = height;
     this.speedX = 0;
     this.speedY = 0;
+    this.score = 0;
   }
 
   icon() {
@@ -49,39 +51,68 @@ class Player {
   }
 }
 
-const devGirl = new Player(0, 225, 50, 50);
+// setting the player and calling the method to print it on canvas
+const devGirl = new Player(0, 225, 80, 80);
 devGirl.icon();
 
-function clear() {
-  // ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  drawBackground();
-  devGirl.icon();
-  lifes();
+let buttonState = true;
+
+function button() {
+  gameTime();
+  // buttonState = !buttonState;
+  if (buttonState) {
+    gameStart.innerHTML = '';
+    buttonState = !buttonState;
+  }
 }
 
+// sets the interval
 function gameTime() {
+  // startGame();
   interval = setInterval(() => {
     updateGame();
     frames += 1;
   }, 15);
 }
 
+// calls the interval when the right answer is clicked
 function rightAnswerInterval () {
   gameTime();
   getTheDiv.innerHTML = '';
-  // new Obstacles.x = 10;
 }
 
-gameTime();
+// Updates and prints the score on canvas
+function updateScore() {
+  if (frames % 20 === 0) {
+    devGirl.score += 1;
+  }
+  ctx.font = '18px serif';
+  ctx.fillStyle = 'white';
+  ctx.fillText("Score: " + devGirl.score, 700, 30);
+}
 
-function updateGame() {
+// let n = 3;
+// lifes();
+
+function clear() {
+  ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  drawBackground();
+  devGirl.icon();
+  lifes();
+}
+
+// game engine
+function updateGame () {
   clear();
   devGirl.newPos();
   createObstacle();
   createQuestion();
+  updateScore();
+
   obstacles.forEach((obst) => {
     obst.drawObstacle();
   });
+
   questions.forEach((questions) => {
     questions.questionMark();
     if (devGirl.crash(questions) && canCrash) {
@@ -90,25 +121,52 @@ function updateGame() {
       clearInterval(interval);
     }
   });
+
   checkGameOver();
 }
 
+// checks if an obstacle hit the player
 function checkGameOver() {
   let crashed = obstacles.some(function (obstacles) {
     return devGirl.crash(obstacles);
   });
 
   if (crashed) {
+    // lifes(n - 1);
     clearInterval(interval);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, 800, 600)
+    ctx.font = '20px serif';
+    ctx.fillStyle = 'gold';
+    ctx.fillText('Game Over');
   }
 } 
 
 function lifes() {
+//   if (n === 3) {
   let img = new Image();
-  img.src = './images/life.png';
+  img.src = './images/life2.png';
   ctx.drawImage(img, 20, 20, 30, 30);
   ctx.drawImage(img, 50, 20, 30, 30);
   ctx.drawImage(img, 80, 20, 30, 30);
+//   } else if (n === 2) {
+//     let img = new Image();
+//     img.src = './images/life2.png';
+//     ctx.drawImage(img, 20, 20, 30, 30);
+//     ctx.drawImage(img, 50, 20, 30, 30);
+//     n -1;
+//   } else if (n === 1) {
+//     let img = new Image();
+//     img.src = './images/life2.png';
+//     ctx.drawImage(img, 20, 20, 30, 30);
+//   } else if (n === 0) {
+    // clearInterval(interval);
+//     ctx.fillStyle = 'black';
+//     ctx.fillRect(0, 0, 800, 600)
+//     ctx.font = '20px serif';
+//     ctx.fillStyle = 'gold';
+//     ctx.fillText('Game Over');
+//   }
 }
 
 document.onkeydown = function (e) {
@@ -149,15 +207,15 @@ class Obstacle {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.width = 60;
-    this.height = 50;
+    this.width = 100;
+    this.height = 80;
     this.name = name;
   }
 
   drawObstacle() {
     this.name = 'iAmABug';
     let img = new Image();
-    img.src = './images/bug-coding.png';
+    img.src = './images/codebug.png';
     ctx.drawImage(img, this.x, this.y, this.width, this.height);
   }
 
@@ -173,11 +231,12 @@ class Obstacle {
   }
 }
 
+// generates obstacles in random positions and takes it out of the array
 function createObstacle() {
-  if (frames % 40 === 0) {
+  if (frames % 100 === 0) {
     const maxPositionY = 400;
     const minPositionY = 100;
-    obstacles.push(new Obstacle(850, Math.floor(Math.random() * (maxPositionY - minPositionY)) + minPositionY));
+    obstacles.push(new Obstacle(800, Math.floor(Math.random() * (maxPositionY - minPositionY)) + minPositionY));
   }
   obstacles.forEach((obst, i) => {
     obst.updateObstacle();
@@ -187,6 +246,7 @@ function createObstacle() {
   })
 }
 
+// generates questions in random positions and takes it out of the array
 function createQuestion() {
   if (frames % 300 === 0) {
     const maxPositionY = 400;
@@ -200,6 +260,7 @@ function createQuestion() {
     }
   });
 }
+
 
 // let maxSize = 200;
 // let minSize = 20;
